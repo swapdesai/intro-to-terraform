d5ace2fe68759b646d278204f89e41f1b549a3c9
Desai, Swapnil (AU - Melbourne) <swadesai@deloitte.com.au>
2018-04-07 12:28:09 +1000

    added S3 as remote backend including locking and consistency

--- main.tf
+++ main.tf
@@ -10,6 +10,21 @@ provider "aws" {
   region     = "ap-southeast-2"
 }
 
+# ------------------------------------------------------------------------------
+# Configure the AWS S3 as remote backend inclusing locking and consistency
+# ------------------------------------------------------------------------------
+terraform {
+  backend "s3" {
+    bucket = "intro-to-terraform-remote-state-storage"
+    key    = "terraform.tfstate"
+    region = "ap-southeast-2"
+
+    # (Optional) The name of a DynamoDB table to use for state locking and consistency.
+    # The table must have a primary key named LockID. If not present, locking will be disabled.
+    # dynamodb_table = <<dynamodb_table>>
+
+  }
+}
 
 # Server port EC2 insatnce should listen on
 variable "server_port" {
